08079e8687f4acee90848f8994494457d246f89a
Sanjide <sanjidmsnjdp@gmail.com>
2025-11-13 17:32:02 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-// 'auth' കൂടി ഇമ്പോർട്ട് ചെയ്യുന്നു
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+// ഈ ഫയൽ ഫയർബേസുമായി കണക്ട് ചെയ്യുന്നു.
+// എല്ലാ JS ഫയലുകളും ഈ ഫയലിനെയാണ് ആശ്രയിക്കുന്നത്.
 
-// നിങ്ങളുടെ ഫയർബേസ് കോൺഫിഗറേഷൻ
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore, setLogLevel } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// നിങ്ങളുടെ ഫയർബേസ് കോൺഫിഗറേഷൻ വിവരങ്ങൾ
 const firebaseConfig = {
     apiKey: "AIzaSyCePcVE_BTiFuYXAApNmbMKHdkhQ9Ay_F4",
     authDomain: "al-ambar-perfume-company.firebaseapp.com",
@@ -14,12 +16,12 @@ const firebaseConfig = {
     measurementId: "G-39XB0WLW6P"
 };
 
-// Initialize Firebase
+// ഫയർബേസ് ആരംഭിക്കുന്നു
 const app = initializeApp(firebaseConfig);
 
-// db, auth എന്നിവ ഇനിഷ്യലൈസ് ചെയ്യുന്നു
-const auth = getAuth(app);
-const db = getFirestore(app);
+// ഫയർബേസ് സേവനങ്ങൾ എക്സ്പോർട്ട് ചെയ്യുന്നു
+export const db = getFirestore(app); // ഡാറ്റാബേസ് (Firestore)
+export const auth = getAuth(app);    // ലോഗിൻ (Authentication)
 
-// **** 'db'-യോടൊപ്പം 'auth' കൂടി എക്സ്പോർട്ട് ചെയ്യുന്നു ****
-export { db, auth };
+// ഡീബഗ്ഗിംഗ് ലോഗുകൾ കാണാൻ
+setLogLevel('Debug');
